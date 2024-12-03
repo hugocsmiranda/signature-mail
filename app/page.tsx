cf999e8f2b4def5160bedfd71a0d84910c25ffc3
Hugo Miranda <44051923+hugocsmiranda@users.noreply.github.com>
2024-12-03 11:49:44 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,16 @@ export default function Home() {
     setSubmittedData(data);
   };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex">
-          <h1 className="text-4xl font-extrabold !leading-normal tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-transparent bg-clip-text bg-gradient-to-r to-blue-500 from-blue-800">
-            Signature email
-          </h1>
-        </div>
-        <div className="flex space-x-8 w-full">
+    <div className="grid grid-rows-[0px_1fr_0px] items-center justify-items-center min-h-screen">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full px-16">
+        <div className="flex space-x-8 w-full justify-evenly">
+          <div className="flex flex-col gap-8 w-2/5">
+            <h1 className="text-4xl font-extrabold !leading-normal tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-transparent bg-clip-text bg-gradient-to-r to-blue-500 from-blue-800">
+              Signature email
+            </h1>
+            <ProfileForm onSubmit={handleFormSubmit} />
+          </div>
           {/* Composant Formulaire */}
-          <ProfileForm onSubmit={handleFormSubmit} />
 
           {/* Composant Résultat */}
           {submittedData && <FormResult data={submittedData} />}
